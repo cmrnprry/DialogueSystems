a7057213a12ced2457591f8b289e0dc18b4538fc
cmrnprry <cmrnprry@gmail.com>
2021-04-02 10:49:54 -0400

    added animations and speech bubbles

--- Assets/Scripts/Two way/TwoWaySystem.cs
+++ Assets/Scripts/Two way/TwoWaySystem.cs
@@ -8,12 +8,14 @@ using TMPro; //NEED TO IMPORT THIS TO USE TMPRO
 public class TwoWaySystem : MonoBehaviour
 {
     public GameObject textbox; //Text box that will be the parent
+    public Animator box;
     public TextMeshProUGUI text; //Text Gameobject that  will hold the text
     public TextMeshProUGUI speakerTag; //speaker text Gameobject that  will hold the text
 
     private Dialogue[] dialogue; //array of dialogue
     private int index = 1; //What line of dialogue we're on
     private bool isPlayingDialogue = false; //check if we should be playing dialogue
+    private string lastTalk = "";
 
     // Start is called before the first frame update
     void Start()
@@ -27,10 +29,14 @@ public class TwoWaySystem : MonoBehaviour
     public void TurnOffDialogue()
     {
         // Clear the text and turn off the textbox
+        box.SetBool("Turn On", false);
         text.text = "";
         speakerTag.text = "";
-        textbox.SetActive(false);
         isPlayingDialogue = false;
+
+        // Turn Off the speech bubble at the last index
+        // Remember that arrays are 0 indexed so the last index is the length minus 1
+        dialogue[dialogue.Length - 1].speechBubble.SetActive(false);
         index = 1;
     }
 
@@ -38,9 +44,13 @@ public class TwoWaySystem : MonoBehaviour
     {
         dialogue = dia;
         textbox.SetActive(true);
+        box.SetBool("Turn On", true);
         isPlayingDialogue = true;
         text.text = dialogue[0].words;
         speakerTag.text = dialogue[0].name;
+
+        //Turn on the first speech bubble
+        dialogue[0].speechBubble.SetActive(true);
     }
 
     // Update is called once per frame
@@ -50,8 +60,12 @@ public class TwoWaySystem : MonoBehaviour
         {
             if (index < dialogue.Length)
             {
+                //Turn off the previous speech bubble and turn on the current one
+                dialogue[index - 1].speechBubble.SetActive(false);
+                dialogue[index].speechBubble.SetActive(true);
                 text.text = dialogue[index].words;
                 speakerTag.text = dialogue[index].name;
+                
                 index++;
             }
         }
--- Assets/Scripts/Two way/TwoWayText.cs
+++ Assets/Scripts/Two way/TwoWayText.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 [System.Serializable]
 public struct Dialogue //This is a struct. Please do not be afraid
-{
+{ 
+    
     public string name;
+    public GameObject speechBubble;
     [TextArea(3, 5)]
     public string words;
+   
 }
+
 public class TwoWayText : MonoBehaviour
 {
     //An array of structs
